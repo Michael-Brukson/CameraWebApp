d6225dcd4ea7bf728cf9996a2e00589266c54428
Michael <michael.brukson@my.liu.edu>
2025-07-01 18:51:21 -0400

    added serverside and clientside route, fixed JS. Next steps, make pretty then package in application.

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-clients = {}
+SERVER = 'server'
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index() -> str:
+    return render_template('client.html')
+
+
+@app.route('/server')
+def server() -> str:
+    return render_template('server.html')
+
+@socketio.on('join_server')
+def on_join_server() -> None:
+    join_room(SERVER)
+
+@socketio.on('disconnect')
+def on_disconnect() -> None:
+    emit('user_disconnected', {'sid': request.sid}, room=SERVER)
 
 @socketio.on('video_frame')
-def handle_video_frame(data):
-    emit('new_video_frame', data, broadcast=True, include_self=False)
+def on_video_frame(data) -> None:
+    data['sid'] = request.sid
+    emit('local_frame', data, room=request.sid)
+    emit('server_frame', data, room=SERVER) 
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
---
+++ static/js/client.js
@@ -0,0 +1,96 @@
+var socket = io();
+
+socket.on('local_frame', function(data) {
+  var remoteFeedDiv = document.getElementById('remote-feed');
+  var remoteCanvas = remoteFeedDiv.querySelector('canvas');
+
+  var img = new window.Image();
+  img.onload = function() {
+    remoteCanvas.width = img.width;
+    remoteCanvas.height = img.height;
+    var ctx = remoteCanvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+  };
+  img.src = data.image;
+});
+
+socket.on('user_disconnected', function(data) {
+  var sid = data.sid;
+  var canvasId = 'remote-feed-canvas-' + sid;
+  var remoteCanvas = document.getElementById(canvasId);
+  if (remoteCanvas) {
+    remoteCanvas.remove();
+  }
+});
+
+var interval = null;
+var activeStream = null;
+var activeVideo = null;
+var activeCanvas = null;
+
+var startBtn = document.getElementById('start-transmit');
+
+startBtn.addEventListener('click', function() {
+  if (startBtn.dataset.state === 'transmitting') {
+    if (interval) {
+      clearInterval(interval);
+      interval = null;
+    }
+    if (activeStream) {
+      activeStream.getTracks().forEach(track => track.stop());
+      activeStream = null;
+    }
+    if (activeVideo) {
+      activeVideo.remove();
+      activeVideo = null;
+    }
+    if (activeCanvas) {
+      activeCanvas.remove();
+      activeCanvas = null;
+    }
+    startBtn.textContent = 'Start';
+    startBtn.dataset.state = 'stopped';
+    return;
+  }
+
+  var facingMode = "user";
+  var constraints = {
+    audio: false,
+    video: {
+      facingMode: facingMode
+    }
+  };
+
+  navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+    activeStream = stream;
+    var video = document.createElement('video');
+    video.setAttribute('playsinline', '');
+    video.setAttribute('autoplay', '');
+    video.setAttribute('muted', '');
+    video.style.display = 'none';
+    document.body.appendChild(video);
+    video.srcObject = stream;
+    activeVideo = video;
+
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    document.getElementById('remote-feed').appendChild(canvas);
+    activeCanvas = canvas;
+
+    video.onloadedmetadata = function() {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+    }
+
+    interval = setInterval(function() {
+      if (canvas.width && canvas.height) {
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        var dataURL = canvas.toDataURL('image/jpeg');
+        socket.emit('video_frame', { image: dataURL });
+      }
+    }, 41.6); // ~24fps
+
+    startBtn.textContent = 'Stop';
+    startBtn.dataset.state = 'transmitting';
+  });
+});
