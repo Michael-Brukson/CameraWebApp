82bc8103958773a9885a95bcab326a15a599576c
Michael <michael.brukson@my.liu.edu>
2025-07-03 15:01:12 -0400

    updated requirements.txt, added pyvirtualcam.

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
+import pyvirtualcam as pvc
+import base64
+import numpy as np
+import cv2
+import re
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-SERVER = 'server'
+SERVER: str = 'server'
+cam = None
 
 @app.route('/')
 def index() -> str:
     return render_template('client.html')
-
 
 @app.route('/server')
 def server() -> str:
@@ -25,6 +30,20 @@ def on_disconnect() -> None:
 
 @socketio.on('video_frame')
 def on_video_frame(data) -> None:
+    global cam
+
+    frame = data['image']
+    frame = re.sub('^data:image/.+;base64,', '', frame) # str
+    frame = base64.b64decode(frame) # convert to bytes
+
+    frame = np.frombuffer(frame, dtype=np.uint8) # convert to np.ndarray
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+
+    if cam is None:
+        cam = pvc.Camera(width=frame.shape[1], height=frame.shape[0], fps=20, fmt=pvc.PixelFormat.BGR)
+
+    cam.send(frame)
+
     data['sid'] = request.sid
     emit('local_frame', data, room=request.sid)
     emit('server_frame', data, room=SERVER) 
